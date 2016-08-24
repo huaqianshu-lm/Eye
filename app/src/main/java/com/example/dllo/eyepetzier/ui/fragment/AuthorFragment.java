cd1b5cd353a8c35d353e6d1fe0d5d776c7015876
李淼 <1374113170@qq.com>
2016-08-24 11:55:01 +0800

    完成动画

--- app/src/main/java/com/example/dllo/eyepetzier/ui/fragment/AuthorFragment.java
+++ app/src/main/java/com/example/dllo/eyepetzier/ui/fragment/AuthorFragment.java
@@ -90,10 +90,6 @@ public class AuthorFragment extends AbaBaseFragment {
                             final AuthorFragmentBean.ItemListBean.DataBean.HeaderBean headerBean = dataBean.getHeader();
 
                             final List<AuthorFragmentBean.ItemListBean.DataBean.VideoItemListBean> videoItemListBeen = itemListBean.getData().getItemList();
-<<<<<<< HEAD
-=======
-
->>>>>>> bd364a37d2f536fb787e92e0a514cad6c7bafdc5
                             holder.setText(R.id.item_author_fragment_title_tv, headerBean.getTitle());
                             holder.setText(R.id.item_author_fragment_subtitle_tv, headerBean.getSubTitle());
                             holder.setText(R.id.item_author_fragment_description_tv, headerBean.getDescription());
@@ -119,13 +115,10 @@ public class AuthorFragment extends AbaBaseFragment {
                                         @Override
                                         public void onClick(View v) {
                                             Bundle bundle = new Bundle();
-<<<<<<< HEAD
                                             bundle.putParcelable(Contant.AUTHOR_TO_VIDEO, dataBean);
                                             bundle.putInt(Contant.VIDEO_POS,pos);
-=======
 //                                            bundle.putParcelableArrayList(Contant.AUTHOR_TO_VIDEO, (ArrayList<? extends Parcelable>) videoItemListBeen);
                                             bundle.putParcelable(Contant.AUTHOR_TO_VIDEO, dataBean);
->>>>>>> bd364a37d2f536fb787e92e0a514cad6c7bafdc5
                                             goTo(context, VideoIntroduceActivity.class, bundle);
                                             T.shortMsg("作者界面视频图片的点击事件,跳转到视频的详情界面");
 
@@ -139,9 +132,7 @@ public class AuthorFragment extends AbaBaseFragment {
                                     Log.e("AuthorFragment", "getId():" + itemListBean.getData().getHeader().getId());
                                     Bundle bundle = new Bundle();
                                     bundle.putParcelableArrayList(Contant.AUTHOR_TO_SORT, (ArrayList<? extends Parcelable>) itemListBeen);
-<<<<<<< HEAD
                                     goTo(context, VideoIntroduceActivity.class, bundle);
-=======
                                     goTo(context, VideoIntroduceActivity.class,bundle);
 
                                     String urlDate = NetUrl.AUTHOR_2ND_DETAIL_URL_START
@@ -158,7 +149,6 @@ public class AuthorFragment extends AbaBaseFragment {
                                     bundle.putString(NetUrl.KEY_LOGO, headerBean.getIcon());
                                     goTo(getActivity(), Author2ndDetailActivity.class, bundle);
 
->>>>>>> bd364a37d2f536fb787e92e0a514cad6c7bafdc5
                                     T.shortMsg("作者界面的item点击事件,跳转到排序界面");
                                 }
                             });
